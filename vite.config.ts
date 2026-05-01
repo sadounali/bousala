eab2a472d47999bcb0ffe3673d2ab5c61c4ebfc8
sadounali <3alisadoun@gmail.com>
2026-05-01 19:31:23 +0100

    Update base URL based on Vercel environment

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
     plugins: [react(), tailwindcss()],
-    base: '/bousala/',
+    base: process.env.VERCEL ? '/' : '/bousala/',
     define: {
 'process.env.GEMINI_API_KEY': JSON.stringify(process.env.GEMINI_API_KEY || env.GEMINI_API_KEY),    },
     resolve: {
